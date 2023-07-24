f33ac3ab259c0161c91f1c580c983ae4723f7e39
Juan Bautista Correa <jbautistacorrea@gmail.com>
2023-07-24 16:45:01 -0300

    Commit del día 24-07-2023

--- calculadora/deduccion/views.py
+++ calculadora/deduccion/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from .models import Deduccion
 
 def deducir(request):
-
-    return render(request, 'deduccion/deduccion.html')
+    d=Deduccion()
+    return render(request, 'deduccion/deduccion.html', {"d", d})
