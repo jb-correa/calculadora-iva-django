6bf98d58ac6d2f17d42494c1c048509552b714e5
Juan Bautista Correa <jbautistacorrea@gmail.com>
2023-10-27 09:41:49 -0300

    Cambios en html y diseño

--- calculadora/suma/views.py
+++ calculadora/suma/views.py
@@ -6,10 +6,16 @@ def suma(request):
     if request.method == 'POST':
         form = SumaForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('suma')
+            suma=form.save(commit=False)
+            suma.resultado=suma.precio+(suma.precio*suma.porcentaje/100)
+            suma.save()
+            return render(request, 'suma/sumado.html', {'suma': suma})
     else:
         form = SumaForm()
     return render(request, 'suma/suma.html', {'form': form})
     
  
+def sumado (request):
+    sumas=Suma.objects.all()
+    suma=sumas[0]
+    return render(request,'suma/sumado.html',{'suma': suma})
