c0a2b5a8753c991ce262c62a78f546fda49f80c6
Juan Bautista Correa <jbautistacorrea@gmail.com>
2023-10-27 09:29:40 -0300

    Cambios en el html y diseño

--- calculadora/suma/views.py
+++ calculadora/suma/views.py
@@ -1,21 +1,15 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect, render
 from .models import Suma, SumaForm
-from django.contrib import messages
 
-def suma(request):
-    form=SumaForm()
-    if request.method =='POST':
-        form=SumaForm(data=request.POST)
+def sumar(request):
+    form = SumaForm()
+    if request.method == 'POST':
+        form = SumaForm(request.POST)
         if form.is_valid():
-            suma=form.save(commit=False)
-            suma.resultado=suma.precio+(suma.precio*suma.porcentaje/100)
-            suma.save()      
-            return render(request, 'suma/sumado.html', {'suma': suma})
+            form.save()
+            return redirect('suma')
     else:
-         form=SumaForm()
-    return render(request, 'suma/suma.html', {"form": form})
-
-def sumado(request):
-    sumas=suma.objects.all()
-    suma=sumas[0]
-    return render(request, 'suma/sumado.html', {'suma', suma})
+        form = SumaForm()
+    return render(request, 'suma/suma.html', {'form': form})
+    
+ 
